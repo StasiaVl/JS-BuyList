70225e0965759e65743f5a9cfb39046251f411c2
StasiaVl <strange-stasia@ukr.net>
2017-10-10 23:58:49 +0300

    Home work 3
    7 task

--- main.js
+++ main.js
@@ -1,43 +1,66 @@
 $(function () {
     var $list = $(".bl-list");
     var ONE_ROW_HTML = $(".one-row").html();
+    var $left_list = $(".bl-left-list");
+    var $bought_list = $(".bl-bought-list");
+    var ONE_ITEM_HTML = $(".one-item").html();
 
     function addItem(title) {
         var $node = $(ONE_ROW_HTML);
+        var $nodeLeft = $(ONE_ITEM_HTML);
+        var $nodeBought = $(ONE_ITEM_HTML);
+        $nodeBought.find(".label").css("textDecoration", "line-through");
+        $nodeBought.find(".name").css("textDecoration", "line-through");
+        $nodeBought.hide();
         var quantity = 1;
         var $quantity_label = $node.find(".label");
-        var name = $node.find(".left");
+        var $quantity_left_label = $nodeLeft.find(".count");
+        var $quantity_bought_label = $nodeBought.find(".count");
+        var $name = $node.find(".left");
+        var $name_left = $nodeLeft.find(".name");
+        var $name_bought= $nodeBought.find(".name");
+
+        $name.text(title);
+        $name_left.text(title);
+        $name_bought.text(title);
         $quantity_label.text(quantity);
-        name.text(title);
+        $quantity_left_label.text(quantity);
+        $quantity_bought_label.text(quantity);
 
         $node.find(".remove").click(function () {
             $node.remove();
+            $nodeLeft.remove();
+            $nodeBought.remove();
         });
 
-        name.click(function () {
-            name.hide();
+        $name.click(function () {
+            $name.hide();
             $node.find(".input-text").show();
             $node.find(".input-text").val(title);
             $node.find(".input-text").focus();
         });
         $node.find(".input-text").focusout(function () {
-            name.show();
+            $name.show();
             $node.find(".input-text").hide();
 
             if ($node.find(".input-text").val().trim()) {
                 title = $node.find(".input-text").val();
-                name.text(title);
+                $name.text(title);
+                $name_left.text(title);
+                $name_bought.text(title);
             }
         });
         $node.find(".input-text").keyup(function (e) {
             if (e.which == 13) {
-                name.show();
+                $name.show();
 
                 $node.find(".input-text").hide();
 
                 if ($node.find(".input-text").val().trim()) {
                     title = $node.find(".input-text").val();
-                    name.text(title);
+                    $name.text(title);
+                    $name_left.text(title);
+                    $name_bought.text(title);
                 }
             }
         });
@@ -48,6 +71,8 @@ $(function () {
                 $node.find(".minus").prop("disabled", false);
             }
             $quantity_label.text(quantity);
+            $quantity_left_label.text(quantity);
+            $quantity_bought_label.text(quantity);
         });
         $node.find(".minus").click(function () {
             quantity -= 1;
@@ -55,6 +80,8 @@ $(function () {
                 $node.find(".minus").prop("disabled", true);
             }
             $quantity_label.text(quantity);
+            $quantity_left_label.text(quantity);
+            $quantity_bought_label.text(quantity);
         });
 
         $node.find(".buy").click(function () {
@@ -63,7 +90,9 @@ $(function () {
                 $node.find(".add-buttons").css("visibility", "hidden");
                 $node.find(".unbuy").show();
                 $node.find(".buy").hide();
-                name.css("textDecoration", "line-through")
+                $name.css("textDecoration", "line-through")
+                $nodeLeft.hide();
+                $nodeBought.show();
             });
             $node.fadeIn("normal");
         });
@@ -73,12 +102,16 @@ $(function () {
                 $node.find(".add-buttons").css("visibility", "visible");
                 $node.find(".unbuy").hide();
                 $node.find(".buy").show();
-                name.css("textDecoration", "none")
+                $nodeLeft.show();
+                $nodeBought.hide();
+                $name.css("textDecoration", "none")
             });
             $node.fadeIn("normal");
         });
 
         $list.append($node);
+        $left_list.append($nodeLeft);
+        $bought_list.append($nodeBought);
     }
 
 
